e9c03ec9b358a80ffd2750cb0d9c2207aea14491
Henry Kevin <kevrat21@gmail.com>
2025-10-08 13:20:02 +0700

    Kejar 1 - Soal 1

---
+++ src/Kejar_1/Perkenalan_Mahasiswa_1.java
@@ -0,0 +1,13 @@
+package Kejar_1;
+
+public class Perkenalan_Mahasiswa_1 {
+
+    static void main(String[] args) {
+        System.out.println("Nama : Budi Santoso");
+        System.out.println("NIM : 123456789");
+        System.out.println("Program Studi : Teknik Informatika");
+        System.out.println("Fakultas : Ilmu Komputer");
+        System.out.println("No. HP : 081234567890");
+        System.out.println("Asal Kota : Jakarta");
+    }
+}
