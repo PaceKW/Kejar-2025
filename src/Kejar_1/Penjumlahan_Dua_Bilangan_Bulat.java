4222706c66c63fb436132dc99d60549c1a4483d3
Henry Kevin <kevrat21@gmail.com>
2025-10-08 13:33:45 +0700

    Kejar 1 - Soal 3

---
+++ src/Kejar_1/Penjumlahan_Dua_Bilangan_Bulat.java
@@ -0,0 +1,16 @@
+package Kejar_1;
+
+import java.util.Scanner;
+
+public class Penjumlahan_Dua_Bilangan_Bulat {
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+
+        System.out.println("Hasil Penjumlahan: " + c);
+    }
+}
