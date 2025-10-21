7bbe14edf454710325e592eb9ae71551b1907d79
Henry Kevin <kevrat21@gmail.com>
2025-10-22 02:59:24 +0700

    Kejar 2 - Soal 1

---
+++ src/Kejar_2/Even_Odd.java
@@ -0,0 +1,25 @@
+package Kejar_2;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+
+        // TERNARY
+        System.out.print((a%2==0) ? "Even" : "Odd");
+
+        // IF ELSE
+//        if (a%2==1){
+//            System.out.print("Odd");
+//        }else {
+//            System.out.print("Even");
+//        }
+        input.close();
+
+    }
+}
